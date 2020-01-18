ac76cd237d72e254c0a5fc07b0bb73a44c50a478
mischa <rilakkuma0330k@gmail.com>
2020-01-18 20:50:18 +0800

    [add] use loader convert css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import './all.css'
+
 console.log("Hello Webpack !")
 console.log("edit me, use watch script can keep :D")
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
   }
 };
 
 console.log(`[NODE_ENV] ${process.env.NODE_ENV}`)
+
+// loader(有順序性，由後往前執行 css-loader > style-loader)
+// css-loader  : 把 css 語法轉成 webpack 看的懂的 javascript語法
+// style-loader: 把 webpack 處理好的 css 樣式注入在 style tag 裡
